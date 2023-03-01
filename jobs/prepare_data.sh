24e86e2179b62db782d6167367594e72aab5c0a5
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-03-01 09:39:38 +0100

    Add scripts for running/debugging jobs on the cluster

---
+++ jobs/prepare_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p ml_gpu-rtx2080
+#SBATCH --gres=gpu:1
+#SBATCH -t 1-00:00:00 # time (D-HH:MM)
+#SBATCH -c 2 # number of cores
+#SBATCH -o logs/%j.%x.%N.out # STDOUT  (the folder log has to be created prior to running or this won't work)
+#SBATCH -e logs/%j.%x.%N.err # STDERR  (the folder log has to be created prior to running or this won't work)
+#SBATCH -J PrepareData # sets the job name. If not specified, the file name will be used as job name
+
+if [[ $# -eq 0 ]]; then
+    echo "No flag provided, preparing shakespeare data (GPT2 tokenization)"
+    python data/shakespeare/prepare.py
+elif [[ $1 == "shakespeare_char" ]]; then
+    echo "Preparing shakespeare data (character tokenization)"
+    python data/shakespeare_char/prepare.py
+elif [[ $1 == "openwebtext" ]]; then
+    echo "Preparing openwebtext data (GPT2 tokenization)"
+    python data/openwebtext/prepare.py
+else
+    echo "Invalid flag provided, running file3"
+fi
